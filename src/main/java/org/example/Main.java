1501b363ef2feead767012b695af72a6a3f80612
Burak <burakkgolcuk@gmail.com>
2025-09-08 15:53:26 +0300

    ResultAnalyzer.java değişti

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,25 +3,56 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+         System.out.println(shouldWakeUp(true, -1));
+
+         System.out.println(hasTeen(9, 99, 19));
+         System.out.println(hasTeen(23, 15, 42));
+         System.out.println(hasTeen(22, 23, 34));
+
+         System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+         System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
     }
+
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) return false;
+        return isBarking && (clock < 8 || clock >= 20);
     }
+
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return isTeen(firstAge) || isTeen(secondAge) || isTeen(thirdAge);
     }
+    private static boolean isTeen(int n) {
+        return n >= 13 && n <= 19;
+    }
+
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int upper = isSummer ? 45 : 35;
+        return temp >= 25 && temp <= upper;
     }
+
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) return -1;
+        return width * height;
     }
 
+
     public static double area(double radius) {
-       
+        if (radius < 0) return -1;
+        return radius * radius * Math.PI;
     }
 }
